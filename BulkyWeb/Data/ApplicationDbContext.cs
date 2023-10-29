b7d23faf94f105a84c46c54a4d8303a905937cd0
Georgia Flynn <gflynn2049@michael-flynn.local>
2023-10-29 14:54:34 -0600

    add vscode to gitignore

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Data
 {
-	public class ApplicationDbContext:DbContext
+	public class ApplicationDbContext : DbContext
 	{
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 		{
