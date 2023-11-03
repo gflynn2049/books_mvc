e1c59732ceaef3d2dc9452749a180e08fd1e0cf5
Georgia Flynn <gflynn2049@michael-flynn.local>
2023-11-03 15:38:02 -0600

    update product db

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bulky.Models
 {
@@ -33,6 +34,10 @@ namespace Bulky.Models
         [Display(Name = "Price for for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
 
     }
 }
