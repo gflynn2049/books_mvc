7880bad803015438e0c611dad751b56aaa07c849
Georgia Flynn <gflynn2049@michael-flynn.local>
2023-11-04 23:50:02 -0600

    home page

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Bulky.Models;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.DataAccess.Repository;
 
 namespace BulkyWeb.Areas.Customer.Controllers
 {
@@ -9,15 +11,17 @@ namespace BulkyWeb.Areas.Customer.Controllers
     {
 
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll("Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
