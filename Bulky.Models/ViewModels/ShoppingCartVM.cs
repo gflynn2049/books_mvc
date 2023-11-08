aeb6d19865803c2d6108c366114fc67f1a4af590
Georgia Flynn <gflynn2049@michael-flynn.local>
2023-11-07 20:33:15 -0700

    get summary and load summary UI with data; update SD.cs

--- Bulky.Models/ViewModels/ShoppingCartVM.cs
+++ Bulky.Models/ViewModels/ShoppingCartVM.cs
@@ -4,7 +4,6 @@ namespace Bulky.Models.ViewModels
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
-        public double OrderTotal { get; set; }
-
+        public OrderHeader OrderHeader { get; set; }
     }
 }
