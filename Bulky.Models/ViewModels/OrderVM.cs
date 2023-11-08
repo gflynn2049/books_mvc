bb7852e1ff4b482d7c80f9eb15dcb64f1a5cb27a
Georgia Flynn <gflynn2049@michael-flynn.local>
2023-11-07 22:21:04 -0700

    order list ui

---
+++ Bulky.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,8 @@
+namespace Bulky.Models.ViewModels
+{
+    public class OrderVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetail> OrderDetail { get; set; }
+    }
+}
